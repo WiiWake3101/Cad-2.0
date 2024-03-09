62c821595616e8e2fad01a1ecb79b2a221735384
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-03-09 16:37:27 +0530

    callback changes

--- components/login/Login.jsx
+++ components/login/Login.jsx
@@ -19,60 +19,64 @@ function Login() {
   }
 
   return (
-<main>
-    <section className="container forms">
-      <div className="form login">
-        <div className="form-content">
-          <header>Login</header>
-          <form>
-            <div className="field input-field">
-              <input type="email" placeholder="Email" className="input" />
-            </div>
-            <div className="field input-field">
-              <input
-                type="password"
-                placeholder="Password"
-                className="password"
-              />
-              <i className="bx bx-hide eye-icon"></i>
-            </div>
+    <main>
+      <section className="container forms">
+        <div className="form login">
+          <div className="form-content">
+            <header>Login</header>
+            <form>
+              <div className="field input-field">
+                <input type="email" placeholder="Email" className="input" />
+              </div>
+              <div className="field input-field">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="password"
+                />
+                <i className="bx bx-hide eye-icon"></i>
+              </div>
+              <div className="form-link">
+                <a href="#" className="forgot-pass">
+                  Forgot password?
+                </a>
+              </div>
+              <div className="field button-field">
+                <button>Login</button>
+              </div>
+            </form>
             <div className="form-link">
-              <a href="#" className="forgot-pass">
-                Forgot password?
-              </a>
+              <span>
+                Don't have an account?{" "}
+                <a href="/signup" className="link signup-link">
+                  Signup
+                </a>
+              </span>
             </div>
-            <div className="field button-field">
-              <button>Login</button>
-            </div>
-          </form>
-          <div className="form-link">
-            <span>
-              Don't have an account?{" "}
-              <a href="/signup" className="link signup-link">
-                Signup
-              </a>
-            </span>
+          </div>
+          <div className="line"></div>
+          <div className="media-options">
+            <a
+              href="#"
+              className="field google"
+              onClick={() => signIn("google", { callbackUrl: "/" })}
+            >
+              <img src="#" alt="" className="google-img" />
+              <span>Login with Google</span>
+            </a>
           </div>
         </div>
-        <div className="line"></div>
-        <div className="media-options">
-          <a href="#" className="field google" onClick={() => signIn()}>
-            <img src="#" alt="" className="google-img" />
-            <span>Login with Google</span>
-          </a>
-        </div>
+      </section>
+      <div>
+        <button
+          onClick={() => {
+            signIn("google", { callbackUrl: "/" });
+          }}
+        >
+          Log in
+        </button>
       </div>
-    </section>
-    <div>
-      <button
-        onClick={() => {
-          signIn("google",{ callbackUrl: "/" });
-        }}
-      >
-        Log in
-      </button>
-    </div>
-<main/>
+    </main>
   );
 }
 
