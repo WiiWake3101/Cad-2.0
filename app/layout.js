3a0a07392efbac8be96dd95da753c01e84f857b8
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-03-09 15:49:27 +0530

    added head tag to fix hydration error

--- app/layout.js
+++ app/layout.js
@@ -12,14 +12,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
-      />
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
+        />
+      </head>
       <body className={inter.className}>
         <Nav />
         {children}
